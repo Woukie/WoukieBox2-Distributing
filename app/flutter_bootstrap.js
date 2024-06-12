b24ca4ca4fa2e7ec13fe6fcea5529633542151c2
Woukie <lucaseldon@live.com>
2024-06-12 10:42:00 +0000

    Bot woukiebox github pages deploy

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2555133070"
+    serviceWorkerVersion: "1936048083"
   }
 });
